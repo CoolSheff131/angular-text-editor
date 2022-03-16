11e1638d0f83d2609efee40e748060bee10909f5
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-16 12:58:49 +0800

    register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,19 +10,45 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  fullname = '';
-  email = '';
-  password = '';
-  constructor(private authService: AuthService) {}
+  fullnameCtrl: FormControl;
+  emailCtrl: FormControl;
+  passwordCtrl: FormControl;
+  hide = true;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.fullnameCtrl = new FormControl();
+    this.emailCtrl = new FormControl();
+    this.passwordCtrl = new FormControl();
+  }
 
   ngOnInit(): void {}
 
   register() {
     this.authService
-      .register(this.fullname, this.email, this.password)
-      .subscribe((data: any) => {
-        console.log(data);
-        localStorage.setItem('token', data.token);
-      });
+      .register(
+        this.fullnameCtrl.value,
+        this.emailCtrl.value,
+        this.passwordCtrl.value
+      )
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          localStorage.setItem('token', data.token);
+        },
+        (error) => {
+          console.log(error);
+          this._snackBar.open('Ошибка регистрации', 'Закрыть', {
+            duration: 3000,
+          });
+        }
+      );
+  }
+
+  gotoLogin() {
+    this.router.navigate(['/login']);
   }
 }
